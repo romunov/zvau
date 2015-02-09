1c97f956201234b15f0f23dad06df2e9c7e09386
romunov <romunov@gmail.com>
2015-02-09 16:03:57 +0100

    writeStructure was appending by default, now removed

--- R/writeStructure.R
+++ R/writeStructure.R
@@ -7,16 +7,16 @@
 #' @param genotypes genind A \code{genind} object to be exported.
 #' @param pop Vector A numeric vector which corresponds to population.
 #' @param path Character A character vector of relative or absolute path of the resulting file.
+#' @param ... Arguments passed to \code{write.table}.
 #' 
 #' @return Text file
  
-writeStructure <- function(genotipi, pop, path) {
+writeStructure <- function(genotipi, pop, path, ...) {
   gen <- genind2df(genotipi, sep = " ")
   gen[is.na(gen)] <- "-9 -9"
   output <- cbind(id = row.names(gen), pop = as.numeric(pop), gen[2:ncol(gen)])
   
-  write.table(output, path, sep = " ", row.names = FALSE, col.names = FALSE, 
-              append = TRUE, quote = FALSE)  #,eol='\r')
+  write.table(output, path, sep = " ")
   
   outLegend <- data.frame(id = 1:length(levels(pop)), Pop = levels(pop))
   outLegend
