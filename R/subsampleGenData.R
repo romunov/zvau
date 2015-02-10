ea55afd3eb88a9b20fddf3b43a2157c3c9cdfc4e
romunov <romunov@gmail.com>
2015-02-10 13:09:02 +0100

    subsampleGenData didn't handle locus names well

--- R/subsampleGenData.R
+++ R/subsampleGenData.R
@@ -4,11 +4,11 @@
 #' functions drops levels by reconstructing the object from scratch.
 #' 
 #' @param x A subsetted \code{genind} object, possibly with other fields in \code{other} slot.
-#'  
+
 subsetGenData <- function(x) {
   oth <- lapply(other(x), droplevels)
-  
-  pet <- df2genind(X = genind2df(x, sep = "/"), sep = "/", ind.names = indNames(x),
+  xgen <- genind2df(x, sep = "/")[, locNames(x)]
+  pet <- df2genind(X = xgen, sep = "/", ind.names = indNames(x),
                    loc.names = locNames(x))
   pet@other <- oth
   pet
