6e87d670742d5703ff558fec6bb253572db93bb4
romunov <romunov@gmail.com>
2016-10-24 16:31:11 +0200

    added script and data which runs matching

---
+++ sandbox/do_allele_match.R
@@ -0,0 +1,122 @@
+# Read arguments passed to the script
+args <- commandArgs(trailingOnly = TRUE)
+
+# Display help
+if (any(args %in% "--help")) {
+  cat("
+      --help You are here.
+
+      --input A file (path) to the source file of genotypes, e.g. 'dataset.csv' or './data/dataset.csv' if nested
+              in /data/ relative to the working directory (where script is ran from). File should have a comma as
+              delimiters and tab separating columns. It should have sample name and quality index columns, along 
+              with loci names.
+
+      --alleleMismatch Integer. Number of mismatched alleles (m-hat). Related to matchThreshold and cutHeight.
+
+      --matchThreshold Float. Dissimilarity score which constitutes a match when identifying individuals (s-hat).
+
+      --cutHeight Float. Used for dynamic cutting of the tree by amCluster.
+
+      --output Character. The way results will be output. Can be 'html' or 'csv'.
+
+      --profile Logical. Can be TRUE or FALSE. Profiling is done only if requested. Results in a figures stored
+                locally.
+      --fig Character. Filename of the produced figures. Currently only .png supported.
+      ")
+  return("")
+}
+
+# Load packages
+library(allelematch)
+
+# Process input parameters
+if (any(grepl("--input", x = args))) {
+  input <- args[grepl("--input", x = args)]
+  input <- sub("--input=", "", x = input)
+} else {
+  stop("No input detected. Check path or existance of file.")
+}
+
+if (any(grepl("--alleleMismatch", x = args))) {
+  cmd.amm <- args[grepl("--alleleMismatch", x = args)]
+  cmd.amm <- sub("--alleleMismatch=", "", x = cmd.amm)
+  cmd.amm <- as.numeric(cmd.amm)
+} else {
+  cmd.amm <- NULL
+}
+
+if (any(grepl("--matchThreshold", x = args))) {
+  cmd.mth <- args[grepl("--matchThreshold", x = args)]
+  cmd.mth <- sub("--matchThreshold=", "", x = cmd.mth)
+  cmd.mth <- as.numeric(cmd.mth)
+} else {
+  cmd.mth <- NULL
+}
+
+if (any(grepl("--cutHeight", x = args))) {
+  cmd.cht <- args[grepl("--cutHeight", x = args)]
+  cmd.cht <- sub("--cutHeight=", "", x = cmd.cht)
+  cmd.cht <- as.numeric(cmd.cht)
+} else {
+  cmd.cht <- NULL
+}
+
+if (any(grepl("--output", x = args))) {
+  out <- args[grepl("--output", x = args)]
+  out <- sub("--output=", "", x = out)
+} else {
+  message("No output specified, defaulting to 'default_output.csv'.")
+  out <- "default_output.csv"
+}
+
+if (any(grepl("--profile", x = args))) {
+  do.profile <- args[grepl("--profile", x = args)]
+  do.profile <- sub("--profile=", "", x = do.profile)
+  do.profile <- as.logical(do.profile)
+} else {
+  message("Profiling will not be performed because not not called explicitly.")
+  do.profile <- FALSE
+}
+
+if (all(unlist(lapply(sapply(ls(pattern = "cmd."), FUN = get), FUN = is.null)), !do.profile)) {
+  stop("Please specify at least one parameter (alleleMismatch, matchThreshold or cutHeight), see --help.")
+}
+
+if (any(grepl("--fig", x = args))) {
+  fig.name <- args[grepl("--fig", x = args)]
+  fig.name <- sub("--fig=", "", x = fig.name)
+} else {
+  message("No figure name specified, using default name 'profile_result.png'.")
+  fig.name <- "profile_result.png"
+}
+
+# Import data (can be csv, even SQL statement)
+# Implementing reading from a csv with decimal delimiter and tab as separator. 
+message("Importing dataset...")
+xy <- read.table(input, header = TRUE, dec = ",", sep = "\t")
+
+# Convert the imported dataset into a proper structure for matching
+message("Creating dataset for further processing...")
+d.xy <- amDataset(xy, missingCode = NA, indexColumn = "Samp", metaDataColumn = "QualityIndex") # TODO: add metadata columns
+
+# Do profiling. Figure is saved to working directory.
+if (do.profile) {
+  message("Profiling... This may take a while.")
+  png(fig.name,  width = 500, height = 500)
+  prof <- amUniqueProfile(amDatasetFocal = d.xy, verbose = TRUE)
+  dev.off()
+} else {
+  result <- amUnique(amDatasetFocal = d.xy, 
+                     alleleMismatch = cmd.amm, 
+                     matchThreshold = cmd.mth,
+                     cutHeight = cmd.cht
+                     )
+  
+  # Produce result as specified in output
+  # Catch csv, if not, assume html is requested
+  if (grepl("\\.csv", out)) {
+    amCSV.amUnique(x = result, csvFile = out)
+  } else {
+    amHTML.amUnique(x = result, htmlFile = out)
+  }
+}
